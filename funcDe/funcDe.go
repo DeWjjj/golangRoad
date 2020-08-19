468e64e42f4b592027fa1e029d7feedb2bfe7b16
DeWjjj <wangchaonew123@qq.com>
2020-08-20 01:30:09 +0800

    master

--- funcDe/funcDe.go
+++ funcDe/funcDe.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
+func plus(x, sumY int, y ...int) int { //可增常参数y，实际上是一个数组
+	for _, val := range y {
+		sumY = sumY + val
+	}
+	sum := x + sumY
+	return sum
+}
+
 //函数实际上，都可以写在main里面，实际上是我们把函数单独拿出来封装
 func main() {
-
+	value := plus(1, 2, 3, 4, 5)
+	fmt.Println(value)
 }
