d102c0519d1830c90d485654d3e5c5af05a19e8f
DeWjjj <wangchaonew123@qq.com>
2020-08-26 17:22:11 +0800

    master

---
+++ import_package/import_package.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	plus "golangRoad/plus"
+)
+
+func main() {
+	ret := plus.Plus(1, 2)
+	fmt.Println(ret)
+}
--- plus/plus.go
+++ plus/plus.go
@@ -3,4 +3,5 @@ package plus
 // Plus is exported.
 func Plus(x, y int) int {
 	return x + y
+
 }
