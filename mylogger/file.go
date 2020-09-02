dfb3d2c18fcb1970655f2e383533ccb59740d58c
DeWjjj <wangchaonew123@qq.com>
2020-09-02 18:26:41 +0800

    master

--- mylogger/file.go
+++ mylogger/file.go
@@ -41,12 +41,12 @@ func NewFileLogger(levelStr, fp, fn string, maxSize int64) *FileLogger {
 
 func (f *FileLogger) initFile() error {
 	fullFileName := path.Join(f.filePath, f.fileName)
-	fileObj, err := os.OpenFile(fullFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	fileObj, err := os.OpenFile(fullFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("Open log file failed,err: %v.\n", err)
 		return err
 	}
-	errFileObj, err := os.OpenFile(fullFileName+".err", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	errFileObj, err := os.OpenFile(fullFileName+".err", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("Open log file failed,err: %v.\n", err)
 		return err
@@ -55,6 +55,69 @@ func (f *FileLogger) initFile() error {
 	f.fileObj = fileObj
 	f.errFileObj = errFileObj
 	return nil
+}
+
+func (f *FileLogger) enable(LogLevel LogLevel) bool {
+	return LogLevel >= f.Level
+}
+
+func (f *FileLogger) checkSize(file *os.File) bool {
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Printf("Get file into failed,err: %v.\n", err)
+		return false
+	}
+	return fileInfo.Size() > f.maxFileSize
+	//当前文件大小大于，设置的最大文件大小返回true。
+}
+
+func (f *FileLogger) splitFile(file *os.File) (*os.File, error) {
+	//更改日志名字
+	nowStr := time.Now().Format("20060102150405000")
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Printf("Get file info failed,err: %v.\n", err)
+		return nil, err
+	}
+	logName := path.Join(f.filePath, fileInfo.Name())      //拿到当前日志文件
+	newLogName := fmt.Sprintf("%s.bak%s", logName, nowStr) //拼接一个日志文件备份的名字
+	file.Close()
+	os.Rename(logName, newLogName)
+	//打开一个新的日志文件
+	fileObj, err := os.OpenFile(logName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		fmt.Printf("Open new log file failed,err: %v.\n", err)
+		return nil, err
+	}
+	return fileObj, nil
+}
+
+func (f *FileLogger) log(lv LogLevel, format string, a ...interface{}) {
+	if f.enable(lv) {
+		msg := fmt.Sprintf(format, a...) //组合format和a字段。
+		now := time.Now()
+		funcName, fileName, lineNo := getInfo(3)
+		if f.checkSize(f.fileObj) {
+			newFile, err := f.splitFile(f.fileObj)
+			if err != nil {
+				return
+			}
+			f.fileObj = newFile
+		}
+		fmt.Fprintf(f.fileObj, "[%s] [%s] [%s:%s:%d] %s.\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
+		if lv >= ERROR {
+			if f.checkSize(f.errFileObj) {
+				newFile, err := f.splitFile(f.errFileObj)
+				if err != nil {
+					return
+				}
+				f.errFileObj = newFile
+			}
+			//如果要记录的日志大于等于ERROR级别，我还要在err日志文件中再记录一遍。
+			fmt.Fprintf(f.errFileObj, "[%s] [%s] [%s:%s:%d] %s.\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
+
+		}
+	}
 }
 
 //Newlog func >> exported
@@ -66,24 +129,6 @@ func Newlog(levelStr string) ConsoleLogger {
 	return ConsoleLogger{
 		Level: level,
 	}
-}
-
-func (f *FileLogger) log(lv LogLevel, format string, a ...interface{}) {
-	if f.enable(lv) {
-		msg := fmt.Sprintf(format, a...) //组合format和a字段。
-		now := time.Now()
-		funcName, fileName, lineNo := getInfo(3)
-		fmt.Fprintf(f.fileObj, "[%s] [%s] [%s:%s:%d] %s.\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
-		if lv >= ERROR {
-			//如果要记录的日志大于等于ERROR级别，我还要在err日志文件中再记录一遍。
-			fmt.Fprintf(f.errFileObj, "[%s] [%s] [%s:%s:%d] %s.\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
-
-		}
-	}
-}
-
-func (f *FileLogger) enable(LogLevel LogLevel) bool {
-	return LogLevel >= f.Level
 }
 
 //Debug func >> exported
@@ -116,6 +161,7 @@ func (f *FileLogger) Fatal(format string, a ...interface{}) {
 	f.log(FATAL, format, a...)
 }
 
+//Close func >> exported
 func (f *FileLogger) Close() {
 	f.fileObj.Close()
 	f.errFileObj.Close()
--- mylogger_test/main.go
+++ mylogger_test/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"golangRoad/mylogger"
-	"time"
 )
 
 //testing mylogger.
 func main() {
-	log := mylogger.NewFileLogger("Info", "./", "dew.log", 10*1024*1024)
+	log := mylogger.NewFileLogger("Info", "./", "dew.log", 20*1024*1024)
 	for {
 		log.Debug("Debug log")
 		log.Trace("Trace log")
@@ -17,6 +16,6 @@ func main() {
 		name := "dew"
 		log.Error("Error log.id: %d,name: %s", id, name)
 		log.Fatal("Fatal log")
-		time.Sleep(time.Second * 2)
+		// time.Sleep(time.Second * 2)
 	}
 }
