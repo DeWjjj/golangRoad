9288d750e9bd73aadce115a67a13aa8ba9f4a07b
DeWjjj <wangchaonew123@qq.com>
2020-09-03 20:03:27 +0800

    master

--- reflect04/reflect04.go
+++ reflect04/reflect04.go
@@ -15,10 +15,10 @@ func main() {
 		Name:  "Dew",
 		Score: 100,
 	}
-	t := reflect.TypeOf(stu1)
-	fmt.Println(t.Name(), t.Kind())
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
+	t := reflect.TypeOf(stu1)           //t赋予变量stu1结构体的信息
+	fmt.Println(t.Name(), t.Kind())     //student struct
+	for i := 0; i < t.NumField(); i++ { //NumField()函数可以得到所有结构体中的字段信息
+		field := t.Field(i) //Field()函数可以得到结构体中的字段信息
 		fmt.Printf("name: %s index: %d type: %v json_tag: %v.\n", field.Name, field.Index, field.Type, field.Tag.Get("json"))
 	}
 	if scoreField, ok := t.FieldByName("Score"); ok {
