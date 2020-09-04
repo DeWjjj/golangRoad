3570f648770d74eb0dc6fd84d77a1dc26b82afa0
DeWjjj <wangchaonew123@qq.com>
2020-09-04 13:14:02 +0800

    push

--- ini/ini.go
+++ ini/ini.go
@@ -1,1 +1,21 @@
-package ini
+package main
+
+import "fmt"
+
+//MysqlConfig type >> exported
+type MysqlConfig struct {
+	Address  string `ini:"address"`
+	Port     int    `ini:"port"`
+	Username string `ini:"username"`
+	Password string `ini:"password"`
+}
+
+func loadIni(v interface{}) {
+
+}
+
+func main() {
+	var mc MysqlConfig
+	loadIni(&mc)
+	fmt.Println(mc.Address, mc.Port, mc.Username, mc.Password)
+}
